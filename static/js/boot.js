d901b5e55a3bfab11edb629efb5f7ab7cec5f38f
amerizalde <andrewmerizalde@hotmail.com>
2014-07-09 08:21:26 -0600

    mobile scale testing

--- static/js/boot.js
+++ static/js/boot.js
@@ -26,14 +26,14 @@ BasicGame.Boot.prototype = {
       this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
       this.scale.pageAlignHorizontally = true;
       this.scale.pageAlignVertically = true;
-      this.scale.setScreenSize(true);
+      this.scale.setShowAll();
     } else {
       //  Same goes for mobile settings.
       //  In this case we're saying "scale the game, no lower than 480x800"
       this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-      this.scale.minWidth = 480;
-      this.scale.minHeight = 800;
-      this.scale.startFullScreen();
+      this.scale.pageAlignHorizontally = true;
+      this.scale.pageAlignVertically = true;
+      this.scale.setShowAll();
     }
 
     //  By this point the preloader assets have loaded to the cache, we've set the game settings

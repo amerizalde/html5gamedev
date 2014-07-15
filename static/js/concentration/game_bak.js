893c23df49507ed8e6284a268aaba075c43dca8d
Andrew Merizalde <andrewmerizalde@hotmail.com>
2014-07-15 17:35:45 -0600

    backup game

--- static/js/concentration/game_bak.js
+++ static/js/concentration/game_bak.js
@@ -14,44 +14,28 @@ BasicGame.Game.prototype = {
     this.TILE_W = 141 * this.SCALE;
     this.TILE_H = 190 * this.SCALE;
     this.table = new Array(this.ROWS * this.COLS);
-
     this.pick_one = null;
     this.pick_two = null;
+    this.matchedcount = this.table.length / 2;
 
     this.setTable();
   },
 
   update: function () {
+    if (this.showText) {
+      this.returnText = this.add.text(
+        this.game.width / 2,
+        this.game.height / 2 + 20,
+        'Press Z or Tap Game to go back to Main Menu',
+        {font: '16px Audiowide', fill: '#fff'});
+      this.returnText.anchor.setTo(0.5, 0.5);
+      if (this.input.activePointer.isDown) {
+        this.quitGame();
+      }
+    }
   },
 
   // create-related functions
-
-  render: function () {
-    //  DEBUGGING CODE
-    // this.game.debug.body(this.player);
-  },
-
-  quitGame: function (pointer) {
-    //  Here you should destroy anything you no longer need.
-    //  Stop music, delete sprites, purge caches, free resources, all that good stuff.
-
-    //  Then let's go back to the main menu.
-    this.state.start('MainMenu');
-  },
-
-  pickACard: function (card, pointer) {
-    var suits = ["Clubs", "Hearts", "Diamonds", "Spades"];
-    var ranks = ["A", "K", 'Q', 'J', '10', '9', '8', '7', '6', '5', '4', '3', '2'];
-    // https://stackoverflow.com/questions/4550505/getting-random-value-from-an-array
-    var suit = suits[Math.floor(Math.random() * suits.length)];
-    var rank = ranks[Math.floor(Math.random() * ranks.length)];
-    card.frameName = "card" + suit + rank;
-    console.log(card.frameName);
-    /*
-this.cache._cacheMap.2.cards.frameData._frames.[0 : 54] // the cards
-    */
-  },
-
   setTable: function () {
     var suits = ["Clubs", "Hearts", "Diamonds", "Spades"];
     var ranks = ["A", "K", 'Q', 'J', '10', '9', '8', '7', '6', '5', '4', '3', '2'];
@@ -64,24 +48,53 @@ this.cache._cacheMap.2.cards.frameData._frames.[0 : 54] // the cards
       this.table[this.table.length - i] = cardFrame;
     }
 
-    //set the table
-    // https://stackoverflow.com/questions/4550505/getting-random-value-from-an-array
+    // create a sprite group
     this.cards = this.add.group();
+    // add physics body to all sprites created
+    // must be set before the sprites are added
+    this.cards.enableBody = true;
+    this.cards.physicsBodyType = Phaser.Physics.ARCADE;
+    // create sprites
     this.cards.createMultiple(this.ROWS * this.COLS, "cards", "cardBack_blue2");
+    this.cards.setAll('outOfBoundsKill', true);
+    this.cards.setAll('checkWorldBounds', true);
     this.cards.setAll('inputEnabled', true);
+    // force a property into the sprite object
     this.cards.setAll('reveal', 0, false, false, 0, true);
     for (var i = 0; i < this.ROWS * this.COLS; i++) {
-      // var frame = this.table[i];
+      // get the first sprite not in use.
       var card = this.cards.getFirstExists(false);
+      // set the index here so that it matches up with a index in this.table
+      // for lookup in revealCard()
       card.reveal = i;
+      // move to position
       card.reset(
         i % this.COLS * this.TILE_W,              // x
         Math.floor(i / this.COLS) * this.TILE_H   // y
         );
-      // card.events...
+      // adjust to screen
       card.scale.setTo(this.SCALE, this.SCALE);
+      // add event
       card.events.onInputDown.add(this.revealCard, this);
     }
+  },
+
+  quitGame: function (pointer) {
+    //  Here you should destroy anything you no longer need.
+    //  Stop music, delete sprites, purge caches, free resources, all that good stuff.
+    this.cards.destroy();
+    //  Then let's go back to the main menu.
+    this.state.start('MainMenu');
+  },
+
+  pickACard: function (card, pointer) {
+    var suits = ["Clubs", "Hearts", "Diamonds", "Spades"];
+    var ranks = ["A", "K", 'Q', 'J', '10', '9', '8', '7', '6', '5', '4', '3', '2'];
+    // https://stackoverflow.com/questions/4550505/getting-random-value-from-an-array
+    var suit = suits[Math.floor(Math.random() * suits.length)];
+    var rank = ranks[Math.floor(Math.random() * ranks.length)];
+    // set the frame to a valid name from the 'cards' atlas.
+    card.frameName = "card" + suit + rank;
   },
 
   revealCard: function (card, pointer) {
@@ -91,39 +104,76 @@ this.cache._cacheMap.2.cards.frameData._frames.[0 : 54] // the cards
     }
     // reveal the pick
     card.frameName = this.table[card.reveal];
+    // juice up the interaction
+    card.bringToTop();
+    this.add.tween(card.scale).to(
+      {x: 0.75, y: 0.75}, 100, Phaser.Easing.Bounce.Out, true,
+      0, false, true);
 
     if (this.pick_one === null) {
+      // the first pick
       this.pick_one = card;
     } else {
+      // the second pick
       this.pick_two = this.pick_one;
       this.pick_one = card;
     }
-    console.log("After picks loop");
-    if (this.pick_one && this.pick_two) {
-      console.log("There are two picks. Check for a match.");
-      this.checkMatches();
-      console.log("After matches loop.");
-    }
 
-    console.log("First pick: " + (this.pick_one.frameName || this.pick_one));
-    console.log("Second pick: " + (this.pick_two.frameName || this.pick_two));
+    if (this.pick_one && this.pick_two) {
+      // we have two picks so check for a match
+      this.checkMatches();
+    }
   },
 
   checkMatches: function () {
-    // check for match
       if (this.pick_one.frameName === this.pick_two.frameName) {
-        console.log("There was a match.");
-        this.pick_one.kill();
-        this.pick_two.kill();
+        // MATCH!
+        this.dissolve();
+        // decrement so we know if the game is over
+        this.matchedcount--;
+        if (this.matchedcount <= 0) {
+          this.endGameText(true);
+        }
       } else {
-        console.log("There was not a match.");
+        // NOT A MATCH!
+        return;
       }
   },
 
   flip: function () {
+    // "flipping the cards back over"
     this.pick_one.frameName = "cardBack_blue2";
     this.pick_two.frameName = "cardBack_blue2";
     this.pick_one = null;
     this.pick_two = null;
+  },
+
+  dissolve: function () {
+    // the card sprites are set to kill themselves if they run out of bounds,
+    // so no need to call it explicitly.
+    // this is all the dropping animation.
+    this.pick_one.bringToTop();
+    this.pick_two.bringToTop();
+    this.pick_one.body.reset(this.pick_one.x, this.pick_one.y);
+    this.pick_one.body.velocity.y = 1000;
+    this.pick_two.body.reset(this.pick_two.x, this.pick_two.y);
+    this.pick_two.body.velocity.y = 1000;
+    this.add.tween(this.pick_one.body.velocity.y).to(
+      1000, 250, Phaser.Easing.Back.In, true,
+      0, false, false);
+    this.add.tween(this.pick_two.body.velocity.y).to(
+      1000, 100, Phaser.Easing.Back.In, true,
+      0, false, false);
+    this.add.tween(this.pick_one.scale).to(
+      {x: 0.0, y: 0.0}, 1000, Phaser.Easing.Bounce.Out, true,
+      0, false, false);
+    this.add.tween(this.pick_two.scale).to(
+      {x: 0.0, y: 0.0}, 1000, Phaser.Easing.Bounce.Out, true,
+      0, false, false);
+  },
+
+  endGameText: function (win) {
+    // play again?
+    this.showText = true;
   },
 };
